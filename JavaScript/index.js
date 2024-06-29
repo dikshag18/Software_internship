15e9d8c2b0bcf6bc49503069e90d2b515a4f9840
dikshag18 <guptadiksha203@gmail.com>
2024-06-29 12:24:38 +0530

    Initial Commit

---
+++ JavaScript/index.js
@@ -0,0 +1,16 @@
+// Variables in JavaScript
+// In JavaScript, variables are used to store data that can be referenced and manipulated within a program. There are three main ways to declare a variable in JavaScript.
+// var
+// let
+// const
+
+var name = "Hello World!";
+console.log(name); 
+
+let age = 22;
+console.log(age); 
+
+const country = "India";
+console.log(country); 
+
+
